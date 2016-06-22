5774059dc29db26b0835723a3c0125723eaa141e
Aaron C Gaudette <gaudetteaaron@gmail.com>
2016-06-22 16:20:57 -0400

    Built UserManager and improved demo scene
    UserManager dynamically generates users in the editor (and at runtime),
    swapping in the VR camera/third-person representations as necessary
    Builds are now way less tedious—just swap the user (build) tag on
    UserManager and it regenerates itself
    Demo scene utilizes UserManager and demonstrates unique actor tagging
    (painless persistent player attributes across builds) with colored hats
    example

---
+++ Holojam/Assets/Holojam/Demo/Scripts/ExampleActor.cs
@@ -0,0 +1,21 @@
+﻿//ExampleActor.cs
+//Created by Aaron C Gaudette on 22.06.16
+
+using UnityEngine;
+[RequireComponent(typeof(Holojam.TrackedHeadset))]
+
+public class ExampleActor : MonoBehaviour{
+	//Index of actor (which headset is this?)
+	int actorIndex{get{return (int)(GetComponent<Holojam.TrackedHeadset>().liveObjectTag);}}
+	
+	Renderer hat;
+	public Material[] hatColors = new Material[4];
+	
+	void Start(){
+		hat=transform.GetChild(1).GetComponent<Renderer>();
+	}
+	void Update(){
+		//Display a different color hat for each player, consistent across each user (build)
+		hat.material=hatColors[actorIndex];
+	}
+}
--- Holojam/Assets/Holojam/Tools/UserManager.cs
+++ Holojam/Assets/Holojam/Tools/UserManager.cs
@@ -1,15 +1,43 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//UserManager.cs
+//Created by Aaron C Gaudette on 22.06.16
 
-public class UserManager : MonoBehaviour {
+using UnityEngine;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+namespace Holojam{
+	[ExecuteInEditMode]
+	public class UserManager : MonoBehaviour{
+		public TrackedHeadset user, actor;
+		[Space(8)]
+		public LiveObjectTag userTag = LiveObjectTag.HEADSET1;
+		public int users = 4;
+		
+		void Update(){
+			if(!Application.isPlaying)Start();
+			users=Mathf.Max(users,1);
+		}
+		void Start(){Populate();}
+		
+		void Populate(){
+			Clean();
+			
+			GameObject u = Instantiate(user.gameObject,Vector3.zero,Quaternion.identity) as GameObject;
+			u.transform.parent=this.transform;
+			u.name="User ("+((int)userTag+1)+")";
+			u.GetComponent<TrackedHeadset>().liveObjectTag=userTag;
+			
+			for(int i=0;i<users;++i){
+				if(i==(int)userTag)continue;
+				GameObject a = Instantiate(actor.gameObject,Vector3.zero,Quaternion.identity) as GameObject;
+				a.transform.parent=transform;
+				a.name="Actor ("+(i+1)+")";
+				a.GetComponent<TrackedHeadset>().liveObjectTag=(LiveObjectTag)i;
+			}
+		}
+		void Clean(){
+			foreach(TrackedHeadset t in GetComponentsInChildren<TrackedHeadset>(true))
+				if(Application.isEditor && !Application.isPlaying)
+					DestroyImmediate(t.gameObject);
+				else Destroy(t.gameObject);
+		}
 	}
 }
