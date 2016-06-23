e377ffe2b9c4e6ee2d0b12f994d3b6b2631e9920
Aaron C Gaudette <gaudetteaaron@gmail.com>
2016-06-23 15:58:35 -0400

    Build patch

---
+++ Holojam/Assets/Holojam/Tools/DisableShell.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class DisableShell : MonoBehaviour{
+	Transform shell = null;
+	void Update(){
+		shell=transform.Find("Shell");
+		if(shell!=null)shell.gameObject.SetActive(false);
+	}
+}
