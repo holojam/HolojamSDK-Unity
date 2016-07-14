f4ca43806196ebef96ef36f43dfbaaaf3a08351e
Aaron C Gaudette <gaudetteaaron@gmail.com>
2016-07-13 22:50:21 -0400

    HolojamView debugger

---
+++ Holojam/Assets/Holojam/Tools/HolojamViewDebugger.cs
@@ -0,0 +1,32 @@
+﻿//HolojamViewDebugger.cs
+//Created by Aaron C Gaudette on 13.07.16
+
+using UnityEngine;
+using Holojam.Network;
+
+namespace Holojam{
+	[ExecuteInEditMode, RequireComponent(typeof(HolojamView))]
+	public class HolojamViewDebugger : MonoBehaviour{
+		public string label;
+		
+		public Vector3 rawPosition;
+		public Quaternion rawRotation;
+		public int bits;
+		public string blob;
+		
+		public HolojamView view{get{
+			if(holojamView==null)holojamView=GetComponent<HolojamView>();
+			return holojamView;
+		}}
+		HolojamView holojamView = null;
+		
+		void Update(){
+			view.Label=label;
+			
+			rawPosition=view.RawPosition;
+			rawRotation=view.RawRotation;
+			bits=view.Bits;
+			blob=view.Blob;
+		}
+	}
+}
