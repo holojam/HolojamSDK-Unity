40d10b170f5c847e77c2720b20b67cb69de5a8cc
Aaron C Gaudette <gaudetteaaron@gmail.com>
2016-07-13 22:52:55 -0400

    Initial Messenger writeup

---
+++ Holojam/Assets/Holojam/Demo/Scripts/Messenger.cs
@@ -0,0 +1,26 @@
+﻿//Messenger.cs
+//Created by Aaron C Gaudette on 13.07.16
+//Peer-reviewed in REAL TIME by Daniel W. Zhang (Android Sysops)
+
+using UnityEngine;
+
+[RequireComponent(typeof(TextMesh))]
+public class Messenger : Holojam.Synchronizable{
+	void Reset(){label="Messenger";}
+	
+	public string tmp = "";
+	
+	public TextMesh text{get{
+		if(textMesh==null)textMesh=GetComponent<TextMesh>();
+		return textMesh;
+	}}
+	TextMesh textMesh = null;
+	
+	protected override void Sync(){
+		if(sending){
+			tmp+="a";
+			synchronizedString=tmp;
+		}
+		else tmp=synchronizedString;
+	}
+}
