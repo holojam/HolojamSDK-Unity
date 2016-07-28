50d2730462d3255e0ac78e0601963bf808911d60
Aaron C Gaudette <gaudetteaaron@gmail.com>
2016-07-27 21:37:02 -0400

    IMU data ignored in editor and standalone No more (intended) choppiness Added Utility file

---
+++ Holojam/Assets/Holojam/Server/Utility.cs
@@ -0,0 +1,19 @@
+﻿//Utility.cs
+//Created by Aaron C Gaudette on 27.07.16
+
+using UnityEngine;
+
+namespace Holojam{
+	class Utility{
+		public static bool IsMasterPC(){
+			switch(Application.platform){
+				case RuntimePlatform.OSXEditor: return true;
+				case RuntimePlatform.OSXPlayer: return true;
+				case RuntimePlatform.WindowsEditor: return true;
+				case RuntimePlatform.WindowsPlayer: return true;
+				case RuntimePlatform.LinuxPlayer: return true;
+			}
+			return false;
+		}
+	}
+}
