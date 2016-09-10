fa429991cfc273dd38b7fb2d165cd0902e2f10c6
Aaron C Gaudette <gaudetteaaron@gmail.com>
2016-09-09 22:45:15 -0400

    Synchronizable patch Tweaked implementation of flags--can now toggle sending flag regardless of isMasterPC value Updated comments, made extension more clear

--- Holojam/Assets/Holojam/Server/HolojamView.cs
+++ Holojam/Assets/Holojam/Server/HolojamView.cs
@@ -99,4 +99,3 @@ namespace Holojam.Network {
     }
   }
 }
-
--- Holojam/Assets/Holojam/Tools/Synchronizable.cs
+++ Holojam/Assets/Holojam/Tools/Synchronizable.cs
@@ -5,62 +5,61 @@ using UnityEngine;
 using Holojam.Network;
 
 namespace Holojam.Tools{
-	[ExecuteInEditMode, RequireComponent(typeof(HolojamView))]
-	public class Synchronizable : MonoBehaviour{
-		public string label = "Label";
-		public bool useMasterPC = false;
-		public bool sending = true;
-		
-		//Manage view
-		public HolojamView view{get{
-			if(holojamView==null)holojamView=GetComponent<HolojamView>();
-			return holojamView;
-		}}
-		HolojamView holojamView = null;
-		
-		void UpdateView(){
-			view.Label=label;
-			sending=(sending && !useMasterPC) || (useMasterPC && Utility.IsMasterPC());
-			view.IsMine=sending;
-		}
-		
-		public Vector3 synchronizedVector3{
-			get{return view.RawPosition;}
-			set{if(sending)view.RawPosition=value;}
-		}
-		public Quaternion synchronizedQuaternion{
-			get{return view.RawRotation;}
-			set{if(sending)view.RawRotation=value;}
-		}
-		public int synchronizedInt{
-			get{return view.Bits;}
-			set{if(sending)view.Bits=value;}
-		}
-		public string synchronizedString{
-			get{return view.Blob;}
-			set{if(sending)view.Blob=value;}
-		}
-		
-		//Override these in derived classes
-		protected virtual void Update(){
-			UpdateView(); //Mandatory initialization call
-			
-			//Optional check--you probably don't want to run this code in edit mode
-			if(!Application.isPlaying)return;
-			
-			Sync();
-		}
-		
-		protected virtual void Sync(){
-			//By default syncs transform data
-			if(sending){
-				synchronizedVector3=transform.position;
-				synchronizedQuaternion=transform.rotation;
-			}
-			else{
-				transform.position=synchronizedVector3;
-				transform.rotation=synchronizedQuaternion;
-			}
-		}
-	}
+   [ExecuteInEditMode, RequireComponent(typeof(HolojamView))]
+   public class Synchronizable : MonoBehaviour{
+      public string label = "Label";
+      public bool useMasterPC = false;
+      public bool sending = true;
+
+      //Manage view
+      public HolojamView view{get{
+         if(holojamView==null)holojamView=GetComponent<HolojamView>();
+         return holojamView;
+      }}
+      HolojamView holojamView = null;
+
+      protected void UpdateView(){
+         view.Label=label;
+         view.IsMine = sending && (Utility.IsMasterPC() || !useMasterPC);
+      }
+
+      public Vector3 synchronizedVector3{
+         get{return view.RawPosition;}
+         set{if(sending)view.RawPosition=value;}
+      }
+      public Quaternion synchronizedQuaternion{
+         get{return view.RawRotation;}
+         set{if(sending)view.RawRotation=value;}
+      }
+      public int synchronizedInt{
+         get{return view.Bits;}
+         set{if(sending)view.Bits=value;}
+      }
+      public string synchronizedString{
+         get{return view.Blob;}
+         set{if(sending)view.Blob=value;}
+      }
+
+      protected virtual void Update(){
+         UpdateView(); //Mandatory initialization call
+
+         //Optional check--you probably don't want to run this code in edit mode
+         if(!Application.isPlaying)return;
+
+         Sync();
+      }
+
+      //Override this in derived classes
+      protected virtual void Sync(){
+         //By default syncs transform data
+         if(sending){
+            synchronizedVector3=transform.position;
+            synchronizedQuaternion=transform.rotation;
+         }
+         else{
+            transform.position=synchronizedVector3;
+            transform.rotation=synchronizedQuaternion;
+         }
+      }
+   }
 }
