3d72af75fa4c24c8769277c5422ad6d5fececeea
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-02-20 07:37:48 -0500

    Updated build script basic functionality

--- Dev/Editor/Packager.cs
+++ Dev/Editor/Packager.cs
@@ -1,5 +1,5 @@
-﻿//Packager.cs
-//Created by Aaron C Gaudette on 21.06.16
+﻿// Packager.cs
+// Created by Aaron C Gaudette on 21.06.16
 
 using UnityEngine;
 using UnityEditor;
@@ -7,46 +7,52 @@ using System.IO;
 using System.Collections.Generic;
 
 public class Packager : EditorWindow{
-	[MenuItem("Window/Packager")]
-	public static void ShowWindow(){
-		EditorWindow.GetWindow(typeof(Packager));
-	}
-	
-	string output = "Holojam";
-	string version = "0.0.0";
-	string ignorePath = "_Ignore";
-	bool clear = true;
-	
-	void OnGUI(){
-		output=EditorGUILayout.TextField("Title",output);
-		version=EditorGUILayout.TextField("Version",version);
-		ignorePath=EditorGUILayout.TextField("Ignore Directory",ignorePath);
-		clear=EditorGUILayout.Toggle("Clear on build",clear);
-		if(GUILayout.Button("Build"))Build();
-	}
-	void Build(){
-		List<string> subdirs = new List<string>();
-		string[] paths = Directory.GetDirectories(Application.dataPath);
-		foreach(string p in paths)
-			if(CleanPath(p)!="Assets/"+ignorePath){
-				subdirs.Add(CleanPath(p));
-				//Debug.Log("Packager: Building from "+subdirs[subdirs.Count-1]);
-			}
-		if(subdirs.Count==0){
-			Debug.LogWarning("Packager: No valid build directories");
-			return;
-		}
-		
-		if(clear)
-			foreach(string f in Directory.GetFiles(Directory.GetCurrentDirectory(),"*.unitypackage"))
-				File.Delete(f);
-		AssetDatabase.ExportPackage(
-			subdirs.ToArray(),
-			output+"_v"+version+".unitypackage",
-			ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-		);
-	}
-	string CleanPath(string input){
-		return input.Substring(input.IndexOf("Assets"));
-	}
+  [MenuItem("Window/Packager")]
+  public static void ShowWindow() {
+    EditorWindow.GetWindow(typeof(Packager));
+  }
+
+  string output = "Holojam";
+  string version = "0.0.0";
+  string ignorePath = "Dev";
+  bool clear = true;
+
+  void OnGUI(){
+    output = EditorGUILayout.TextField("Title", output);
+    version = EditorGUILayout.TextField("Version", version);
+    ignorePath = EditorGUILayout.TextField("Ignore Directory", ignorePath);
+    clear = EditorGUILayout.Toggle("Clear on build", clear);
+
+    if (GUILayout.Button("Build")) Build();
+  }
+  void Build(){
+    List<string> subdirs = new List<string>();
+    string[] paths = Directory.GetDirectories(
+      Application.dataPath + "/HolojamSDK-Unity"
+    );
+
+    foreach (string p in paths) {
+      Debug.Log(CleanPath(p) + " vs " + ignorePath);
+      if (CleanPath(p) != "Assets/HolojamSDK-Unity/" + ignorePath)
+        subdirs.Add(CleanPath(p));
+    }
+
+    if (subdirs.Count == 0) {
+      Debug.LogWarning("Packager: No valid build directories");
+      return;
+    }
+
+    if (clear)
+      foreach (string f in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.unitypackage"))
+        File.Delete(f);
+
+    AssetDatabase.ExportPackage(
+      subdirs.ToArray(), 
+      output + "-v" + version + ".unitypackage", 
+      ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
+    );
+  }
+  string CleanPath(string input) {
+    return input.Substring(input.IndexOf("Assets"));
+  }
 }
