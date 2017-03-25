a4172846fdaaa4ac78dd009b9369b7d1bd9527dc
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-03-24 22:28:40 -0400

    Rewrote ViveCalibrator
    Now automatically calibrates each Vive space independently without the
    need for user input or network connection
    - Properly offsets the centroid to fit the physical world and
    standardizes the forward direction (position, rotation)
    Requires ViveModule
    
    Public Calibrate() function
    - Robust, checks validity and logs failure remotely
    
    Pulls lighthouse positions from SteamVR
    - Determines the leading lighthouse using height, within an error
    margin (4cm)
    - Waits for lighthouses to be ‘tracked’ before calibrating
    
    Updated prefab, scene

--- Holojam/Vive/ViveCalibrator.cs
+++ Holojam/Vive/ViveCalibrator.cs
@@ -1,7 +1,8 @@
-﻿// ViveCalibrator.cs
+// ViveCalibrator.cs
 // Created by Holojam Inc. on 01.03.17
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
@@ -9,95 +10,137 @@ using Valve.VR;
 namespace Holojam.Vive {
 
   /// <summary>
-  /// "Calibrates" a Relay space (a room that has been delineated independently, instead of
-  /// a single integrated tracking system) by comparing its tracking centroid to an absolute center.
-  /// Synchronizes a flag (canCalibrate) to control when clients can respond to a calibration
-  /// signal.
+  /// "Calibrates" a Vive space (a room that has been delineated independently, instead of
+  /// a single integrated tracking system) automatically, offsetting its centroid to fit
+  /// within the physical world and standardizing the forward direction.
   /// </summary>
-  [RequireComponent(typeof(ViveControllerReceiver))]
-  public class ViveCalibrator : Holojam.Tools.Synchronizable {
+  [RequireComponent(typeof(ViveModule))]
+  public sealed class ViveCalibrator : MonoBehaviour {
+
+    const float ERROR = .04f; // 4cm
 
     /// <summary>
-    /// A transform representing the tracking bounds, e.g. the CameraRig prefab.
+    /// If not enabled, the component will not attempt to calibrate.
     /// </summary>
-    public Transform centroid;
+    public bool enable = true;
 
-    private ViveControllerReceiver receiver;
-    private Vector3 cachedPosition = Vector3.zero;
+    SteamVR_Events.Action posesAction;
+    Vector3[] lighthouses = new Vector3[2];
+    bool ready = false;
+
+    ViveModule module;
+    Vector3 cachedPosition;
 
     /// <summary>
-    /// If true, a global space calibration will be triggered on button press.
-    /// Set this to true at runtime to allow calibration for all the clients, then
-    /// unset it to disable accidental calibrations. We recommend mapping it to a
-    /// button press or UI element.
+    /// 
     /// </summary>
-    [SerializeField] private bool canCalibrate;
+    public bool Calibrate() {
+      if (!Valid())
+        return false;
 
-    /// <summary>
-    /// Public setter for canCalibrate.
-    /// </summary>
-    public void SetCanCalibrate(bool canCalibrate) {
-      this.canCalibrate = canCalibrate;
-    }
-
-    public override string Label { get { return "RelayCalibrator"; } }
-    public override bool Host { get { return false; } }
-    public override bool AutoHost { get { return true; } }
-
-    protected override void Awake() {
-      base.Awake();
-
-      receiver = GetComponent<ViveControllerReceiver>();
-      if (centroid)
-        cachedPosition = centroid.position;
-    }
-
-    /// <summary>
-    /// If the master client has allowed calibration, check every frame for a "Grip Press Down"
-    /// event from the attached controller receiver.
-    /// </summary>
-    protected override void Update() {
-      base.Update();
-
-      receiver.index = Tools.BuildManager.BUILD_INDEX;
-      receiver.type = ViveModule.Type.RIGHT;
-      receiver.updateTracking = true;
-
-      if (!centroid) {
-        Debug.LogWarning("Holojam.Vive.ViveCalibrator: Centroid not assigned!");
-        return;
+      if (!ready) {
+        Network.RemoteLogger.Log(
+          "Holojam.Vive.ViveCalibrator: Calibration failed, lighthouses not found"
+        );
+        return false;
       }
 
-      if (receiver.GetPressDown(EVRButtonId.k_EButton_Grip) && canCalibrate)
-        // Calibrate to the relative position of the controller
-        Calibrate(receiver.TrackedPosition - centroid.position);
-    }
-
-    /// <summary>
-    /// The only synchronized value is an int, acting as a boolean.
-    /// </summary>
-    public override void ResetData() {
-      data = new Network.Flake(0, 0, 0, 1);
-    }
-
-    /// <summary>
-    /// Synchronize the calibration permission flag.
-    /// </summary>
-    protected override void Sync() {
-      if (Sending) {
-        data.ints[0] = (canCalibrate ? 1 : 0);
-      } else {
-        canCalibrate = (data.ints[0] == 1);
+      if (lighthouses[0].y - lighthouses[1].y < ERROR) { // Relative difference, not absolute
+        Network.RemoteLogger.Log(
+          "Holojam.Vive.ViveCalibrator: Lighthouse heights are too similar to calibrate."
+        );
+        return false;
       }
+
+      if (!module.cameraRig) {
+        Network.RemoteLogger.Log(
+          "Holojam.Vive.ViveCalibrator: Calibration failed, ViveModule camera rig not assigned!"
+        );
+        return false;
+      }
+
+      return DoCalibration(lighthouses[0], lighthouses[1]);
+    }
+
+    bool Valid() {
+      return !Tools.BuildManager.IsMasterClient() && !Tools.BuildManager.IsSpectator() && enable;
+    }
+
+    void Awake() {
+      posesAction = SteamVR_Events.NewPosesAction(OnNewPoses);
+      module = GetComponent<ViveModule>() as ViveModule;
+
+      if (!Valid()) return;
+
+      if (!module.cameraRig) {
+        Debug.Log(
+          "Holojam.Vive.ViveCalibrator: Calibration failed, ViveModule camera rig not assigned!"
+        );
+        return;
+      }
+      cachedPosition = module.cameraRig.transform.localPosition;
+
+      StartCoroutine(WaitToCalibrate());
+    }
+
+    IEnumerator WaitToCalibrate() {
+      // Wait for the lighthouses to become tracked
+      while (!ready) yield return null;
+      Calibrate();
+    }
+
+    bool DoCalibration(Vector3 l0, Vector3 l1) {
+      if (l1.y > l0.y) { // Highest lighthouse is first
+        Vector3 swap = l0;
+        l0 = l1;
+        l1 = swap;
+      }
+
+      l0 = new Vector3(l0.x, 0, l0.z);
+      l1 = new Vector3(l1.x, 0, l1.z);
+
+      // Offset the centroid by its difference to the tracking center
+      Vector3 offset = cachedPosition - .5f * (l0 + l1);
+      module.cameraRig.transform.localPosition = offset;
+
+      // Calculate the forward vector with the corner
+      Vector3 forward = l0 - new Vector3(l0.x, 0, l1.z);
+      module.cameraRig.transform.localRotation = Quaternion.LookRotation(forward);
+
+      Debug.Log(
+        "Holojam.Vive.Calibrator: Calibration successful--Offset = (" + offset.x + ", " + offset.z
+        + "), Forward = (" + forward.x + ", " + forward.z + ")"
+      );
+
+      return true;
     }
 
     /// <summary>
-    /// Offset the centroid by its difference to the absolute center.
+    /// Get the lighthouse positions.
     /// </summary>
-    void Calibrate(Vector3 center) {
-      if (Tools.BuildManager.IsMasterClient() || Tools.BuildManager.IsSpectator())
-        return;
-      centroid.position = cachedPosition - new Vector3(center.x, 0, center.z);
+    void OnNewPoses(TrackedDevicePose_t[] poses) {
+      ready = false;
+
+      // Return if both lighthouses aren't tracked
+      if (poses.Length < 3) return;
+
+      for (int i = 1; i <= 2; ++i) {
+        if (!poses[i].bDeviceIsConnected || !poses[i].bPoseIsValid)
+          return;
+
+        var pose = new SteamVR_Utils.RigidTransform(poses[i].mDeviceToAbsoluteTracking);
+        lighthouses[i-1] = pose.pos;
+      }
+
+      ready = true;
+    }
+
+    void OnEnable() {
+      posesAction.enabled = true;
+    }
+
+    void OnDisable() {
+      posesAction.enabled = false;
     }
   }
 }
