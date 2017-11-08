6f7cde45e972c9f340e033c6f94e938391cb4fa9
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-08 13:36:39 -0500

    Add VirtualObject component stub

---
+++ Holojam/Components/VirtualObject.cs
@@ -0,0 +1,12 @@
+// VirtualObject.cs
+// Created by Holojam Inc. on 08.11.17
+
+using UnityEngine;
+
+namespace Holojam.Components {
+
+  public class VirtualObject : Tools.Trackable {
+
+    //
+  }
+}
