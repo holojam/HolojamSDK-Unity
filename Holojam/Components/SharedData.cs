0efb44dc5a934affd53f663d99541f45bb0b59f8
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-11-08 13:36:32 -0500

    Add SharedData component stub

---
+++ Holojam/Components/SharedData.cs
@@ -0,0 +1,12 @@
+// SharedData.cs
+// Created by Holojam Inc. on 08.11.17
+
+using UnityEngine;
+
+namespace Holojam.Components {
+
+  public class SharedData : Network.Controller {
+
+    //
+  }
+}
